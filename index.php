52f9fc39e0fc5ac08d4da86dbf534eb09596933b
Sergei M <black_white2018@rambler.ru>
2019-12-01 10:18:25 +0300

    Методы

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
     declare(strict_types = 1);
 
-    require_once __DIR__.'/quest/Employee.php';
+    require_once __DIR__.'/quest/User.php';
 
-    $employee1 = new Employee();
-    $employee1 -> name   = 'Иван';
-    $employee1 -> age    = 25;
-    $employee1 -> salary = 1000;
-
-    $employee2 = new Employee();
-    $employee2 -> name   = 'Вася';
-    $employee2 -> age    = 26;
-    $employee2 -> salary = 2000;
-
-    printf("%d<br />", $employee1->salary + $employee2->salary);
-    printf("%d", $employee1->age + $employee2->age);
+    $user = new User;
+    printf("%s", $user->show("hello, world"));
